cd65e68a126795e3147a5347e0e87ca09962f962
Nicolas Lopez Cesan <ncesan@kolektor.com.ar>
2023-09-29 17:53:00 -0300

    addd swagger login controller

--- src/api/auth/auth.controller.ts
+++ src/api/auth/auth.controller.ts
@@ -1,5 +1,15 @@
-import { Controller, Post, Body, HttpException } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
 import { AwsCognitoService } from './aws-cognito.service';
+import { ApiOperation, ApiBody, ApiTags } from '@nestjs/swagger';
+import { IsBoolean, IsString } from 'class-validator';
+
+export class LoginDTO {
+  @IsString()
+  username: string;
+
+  @IsString()
+  password: string;
+}
 
 @Controller('auth')
 export class AuthController {
@@ -7,8 +17,22 @@ export class AuthController {
     private readonly AwsCognitoService: AwsCognitoService,
   ) { }
 
+  @ApiOperation({ summary: 'Login' })
+  @ApiBody({
+    schema: {
+      type: 'object',
+      properties: {
+        username: {
+          type: 'string',
+        },
+        password: {
+          type: 'string',
+        }
+      },
+    },
+  })
   @Post('login')
-  async login(@Body() body: { username: string; password: string }): Promise<any> {
+  async login(@Body() body: LoginDTO): Promise<any> {
     const { username, password } = body;
     return await this.AwsCognitoService.authenticateUser(username, password);
   }
